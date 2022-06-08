027ea9e4ca83a559fc5d1a16e8c4f28ce63999e5
Hellogooys <99305409+Hellogooys@users.noreply.github.com>
2022-06-08 18:12:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from pyrogram.type import InlineKeyboardMarkup, InlineKeyboardButton
 
 nihaal=client(
     "Pyrogram Bot",
-    bot_token="5189137384:AAEhAxlEn9f9wwMHYNJDvxmW-1PkOHHRlD0",
+    bot_token="5541342240:AAFvlATqafF1FB7TtMAMNVqdOv04c1a7nuY",
     api_id="18850696",
     api_hash="e3f5a7b4ea28b5c7ce9fc65e48274ae6"
 )
